e75bc8944a0a0b36c0d6feed292b4f900dd3a54b
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2022-02-24 11:21:39 +0000

    Settings Screen

--- lib/settings_body/settings_body.dart
+++ lib/settings_body/settings_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:out_app/home_body/components/divider.dart';
 
 class SettingsBody extends StatelessWidget {
   const SettingsBody({Key? key}) : super(key: key);
@@ -6,7 +7,78 @@ class SettingsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [],
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: Icon(Icons.account_circle, size: 80))
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "User",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.merge(const TextStyle(color: Color(0xFF000000))),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                child: Text("example@email.com",
+                    style: Theme.of(context).textTheme.bodyText1))
+          ],
+        ),
+        const DividerPadding(),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+          child: TextButton(
+              onPressed: () {},
+              child: Row(children: const [
+                Icon(Icons.chevron_left),
+                Text('Update Account Details')
+              ])),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+          child: TextButton(
+              onPressed: () {},
+              child: Row(
+                  children: const [Icon(Icons.chevron_left), Text('Policy')])),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+          child: TextButton(
+              onPressed: () {},
+              child: Row(
+                  children: const [Icon(Icons.chevron_left), Text('Help')])),
+        ),
+        const DividerPadding(),
+        TextButton(
+          style: TextButton.styleFrom(
+              primary: Theme.of(context).colorScheme.error),
+          onPressed: () {},
+          child: Row(
+            children: const [
+              Icon(
+                Icons.chevron_left,
+              ),
+              Text(
+                'Logout',
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
