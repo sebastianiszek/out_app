79928ca035bdb06724774121462c3a0859b02ae3
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2022-03-09 18:04:40 +0000

    Added firestore

--- lib/search_body/components/search_card.dart
+++ lib/search_body/components/search_card.dart
@@ -1,55 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:out_app/restaurant_screen/restaurant_screen.dart';
 import 'package:out_app/search_body/components/search_card_header.dart';
 import 'package:out_app/search_body/components/search_card_subhead.dart';
 import 'package:out_app/shared_components/img_avatar.dart';
 
 class SearchCard extends StatelessWidget {
-  const SearchCard({Key? key}) : super(key: key);
+  const SearchCard({Key? key, required this.restaurant}) : super(key: key);
+
+  final QueryDocumentSnapshot<Object?> restaurant;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Card(
         margin: const EdgeInsets.fromLTRB(15, 0, 15, 22),
-        decoration: BoxDecoration(
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
-          color: Theme.of(context).colorScheme.background,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 1,
-              blurRadius: 2,
-              offset: const Offset(0, 1), // changes position of shadow
-            ),
-          ],
         ),
-        child: Padding(
-            padding: const EdgeInsets.fromLTRB(12, 10, 10, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                restaurantAvatar(66, 66),
-                Expanded(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        color: Theme.of(context).colorScheme.background,
+
+        // ******* CONTAINER DECORATION *******
+        //
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(15.0),
+        //   color: Theme.of(context).colorScheme.background,
+        //   boxShadow: [
+        //     BoxShadow(
+        //       color: Colors.grey.withOpacity(0.3),
+        //       spreadRadius: 1,
+        //       blurRadius: 2,
+        //       offset: const Offset(0, 1), // changes position of shadow
+        //     ),
+        //   ],
+        // ),
+
+        child: InkWell(
+            borderRadius: BorderRadius.circular(15.0),
+
+            // NAVIGATE
+
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const RestaurantViewScreen(),
+                ),
+              );
+            },
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(12, 10, 10, 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 4, 0, 0),
+                    restaurantAvatar(66, 66),
+                    Expanded(
                         child: Column(
-                          children: const [
-                            //short text if it's too long
-                            SearchCardHeader(title: "Restaurant"),
-                            SearchCardSubHead(
-                                tables: 4,
-                                opened: "Opened Now",
-                                price: 2,
-                                type: "Italian",
-                                rating: 5,
-                                distance: "1 km"),
-                          ],
-                        )),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 4, 0, 0),
+                            child: Column(
+                              children: [
+                                //short text if it's too long
+                                SearchCardHeader(title: restaurant['name']),
+                                SearchCardSubHead(
+                                    tables: restaurant['tables'],
+                                    opened: "Opened Now",
+                                    price: restaurant['price'],
+                                    type: restaurant['type'],
+                                    rating: restaurant['rating'],
+                                    distance: "1 km"),
+                              ],
+                            )),
+                      ],
+                    ))
                   ],
-                ))
-              ],
-            )));
+                ))));
   }
 }
--- lib/search_body/search_body.dart
+++ lib/search_body/search_body.dart
@@ -1,41 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:out_app/home_body/components/divider.dart';
 import 'package:out_app/search_body/components/search_card.dart';
 import 'package:out_app/shared_components/button.dart';
 import 'package:out_app/shared_components/input.dart';
 
-class SearchBody extends StatelessWidget {
+class SearchBody extends StatefulWidget {
   const SearchBody({Key? key}) : super(key: key);
 
   @override
+  _SearchBodyState createState() => _SearchBodyState();
+}
+
+class _SearchBodyState extends State<SearchBody> {
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-          child: TextFormField(
-            decoration: getOutInputDecoration(
-                const Color(0xFFDEE0FF), const Color(0xFF000965)),
-          ),
-        ),
-        const DividerPadding(),
-        const SearchCard(),
-        const SearchCard(),
-        const SearchCard(),
-        Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
-            child: ElevatedButton(
-                onPressed: () {
-                  // Respond to button press
-                },
-                style:
-                    getOutButtonStyle(Theme.of(context).colorScheme.secondary),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text('Notify me of offers',
-                      style: Theme.of(context).textTheme.bodyText1),
-                )))
-      ],
+    final Stream<QuerySnapshot> _restaurantsStream = FirebaseFirestore.instance
+        .collection('Restaurants')
+        .where('name')
+        .snapshots();
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _restaurantsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading..");
+        }
+
+        final data = snapshot.requireData;
+
+        return RestaurantList(data: data);
+      },
     );
   }
 }
+
+class RestaurantList extends StatefulWidget {
+  const RestaurantList({Key? key, required this.data}) : super(key: key);
+  final QuerySnapshot<Object?> data;
+
+  @override
+  _RestaurantListState createState() => _RestaurantListState();
+}
+
+class _RestaurantListState extends State<RestaurantList> {
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    List<QueryDocumentSnapshot<Object?>> list = [];
+
+    for (int i = 0; i < widget.data.size; i++) {
+      if (name.isEmpty) {
+        list = widget.data.docs;
+        break;
+      }
+      if (widget.data.docs[i]['name'].toString().toUpperCase().contains(name)) {
+        list.add(widget.data.docs[i]);
+      }
+    }
+
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+        child: TextFormField(
+            decoration: getOutInputDecoration(
+                const Color(0xFFDEE0FF), const Color(0xFF000965)),
+            onChanged: (String value) {
+              setState(() {
+                name = value.toUpperCase();
+              });
+            }),
+      ),
+      const DividerPadding(),
+      ListView.separated(
+        shrinkWrap: true,
+        itemCount: list.length,
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+        itemBuilder: (context, index) {
+          return SearchCard(
+            restaurant: list[index],
+          );
+          // ListTile(
+          //   leading: const Icon(
+          //     Icons.movie,
+          //     color: Colors.teal,
+          //   ),
+          //   title: Text(list[index]['name']),
+          //   trailing: const Icon(Icons.arrow_forward),
+          //   onTap: () {
+          //     // Navigator.push(
+          //     //   context,
+          //     //   MaterialPageRoute(
+          //     //     builder: (context) => MediaDetailScreen(media: list[index]),
+          //     //   ),
+          //     // );
+          //   },
+          // );
+        },
+      )
+    ]);
+  }
+}
+
+        // Padding(
+        //   padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+        //   child: TextFormField(
+        //     decoration: getOutInputDecoration(
+        //         const Color(0xFFDEE0FF), const Color(0xFF000965)),
+        //   ),
+        // ),
+
+// const SearchCard(),
+
+
+// ****   button   ****
+
+// Padding(
+//             padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+//             child: ElevatedButton(
+//                 onPressed: () {
+//                   // Respond to button press
+//                 },
+//                 style:
+//                     getOutButtonStyle(Theme.of(context).colorScheme.secondary),
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(10),
+//                   child: Text('Notify me of offers',
+//                       style: Theme.of(context).textTheme.bodyText1),
+//                 )))
